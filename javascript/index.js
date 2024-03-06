84734bbaee32bcb08c5dee212c9dac7e534a0e76
luciequant <156855912+luciequant@users.noreply.github.com>
2024-03-06 20:52:41 +0100

    Updated context

--- javascript/index.js
+++ javascript/index.js
@@ -12,7 +12,7 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "5fb1305ca3a3b7a0bo4908f8ftf4371a";
   let context =
-    "You are an expert on quotes, you are so smart and can create so many original quotes. Your goal is to generate at least 2 line quote in basic HTML and separate each line with a <br />. Do not comment it in the beggining and do not show the title.Sign the quote with 'SheCodes AI' inside a <strong> element at the end of the quote. Make sure to use quotation marks";
+    "You are an expert on quotes, you are so smart and can create so many original quotes. Your goal is to generate at least 2 line quote in basic HTML and separate each line with a <br />. Do not comment it in the beggining and do not show the title.Sign the quote with 'SheCodes AI' inside a <strong> element at the end of the quote, separate the signiture with a <br /> . Make sure to use quotation marks";
   let prompt = `Please generate a quote about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayQuote);
